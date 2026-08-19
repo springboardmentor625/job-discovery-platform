dec080c9fa510383c52bf5d935dd5bf979754bbc
MadhuramGarg <01fe24bcs225@kletech.ac.in>
2026-08-19 14:41:35 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,73 +17,73 @@ function App() {
   return (
     <Routes>
 
-      {/* Start */}
+      {}
       <Route
         path="/"
         element={<Start />}
       />
 
-      {/* Register */}
+      {}
       <Route
         path="/register"
         element={<Register />}
       />
 
-      {/* Login */}
+      {}
       <Route
         path="/login"
         element={<Login />}
       />
 
-      {/* Complete Profile */}
+      {}
       <Route
         path="/complete-profile"
         element={<CompleteProfile />}
       />
 
-      {/* Upload Resume */}
+      {}
       <Route
         path="/upload-resume"
         element={<UploadResume />}
       />
 
-      {/* AI Resume Parsing */}
+      {}
       <Route
         path="/resume-parsing"
         element={<ResumeParsing />}
       />
 
-      {/* Extracted Skills & Experience */}
+      {}
       <Route
         path="/resume-analysis"
         element={<ResumeAnalysis />}
       />
 
-      {/* ATS Analysis */}
+      {}
       <Route
         path="/ats-analysis"
         element={<ATSAnalysis />}
       />
 
-      {/* Recommended Jobs */}
+      {}
       <Route
         path="/recommended-jobs"
         element={<RecommendedJobs />}
       />
 
-      {/* Swipe Jobs */}
+      {}
       <Route
         path="/swipe-jobs"
         element={<SwipeJobs />}
       />
 
-      {/* Saved Jobs */}
+      {}
       <Route
         path="/saved-jobs"
         element={<SavedJobs />}
       />
 
-      {/* Right Swipe → Apply + Save + Favorite */}
+      {}
       <Route
         path="/application-success"
         element={<ApplicationSuccess />}
